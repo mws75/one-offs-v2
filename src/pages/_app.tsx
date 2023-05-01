b9d94cbae0d0dbd1f0a65a11de697f3a1d7fe42d
Spencer, Michael <mspencer@oaktreecapital.com>
2023-05-01 07:13:59 -0700

    created

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { type AppType } from "next/app";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+// Check out here for authentication with Clerk - https://clerk.com/docs/nextjs/pages-react
+
+
+// Loads the index.tsx because that is the root page. 
+// Next looks for index.tsx as it's root page. 
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+
+  return(
+    <ClerkProvider {...pageProps} >
+      <Component {...pageProps} />
+    </ClerkProvider>  
+  );  
+};
+
+export default api.withTRPC(MyApp);
