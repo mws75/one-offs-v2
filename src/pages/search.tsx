742e38f1a365ab9681a71e0b22b877575c1f2fae
mws75 <mwspencer75@gmail.com>
2023-06-28 10:39:17 -0700

    searching is working and returning object

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,16 @@
 import { useSearchParams } from "next/navigation";
+import { api } from "~/utils/api";
 
 const SearchPage = () => {
   const search = useSearchParams();
   const searchQuery = search ? search.get("q") : null;
 
   const encodedSearchQuery = encodeURI(searchQuery || "");
+  const { data, isLoading: postsLoading } = api.posts.getByTitle.useQuery({
+    queryString: encodedSearchQuery,
+  });
+
+  console.log(data);
 
   return (
     <>
