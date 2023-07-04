41ee7a946b1ba8e7a56f03e9fa0da6ffc94b0c79
Michael Spencer <mwspencer75@gmail.com>
2023-07-04 14:39:57 -0700

    updated the title and the favicon.ico

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -1,6 +1,12 @@
-import type { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 
 export const PageLayout = (props: PropsWithChildren<{}>) => {
+  useEffect(() => {
+    document.title = "Byte-Size-Tech";
+    const favicon = document.getElementById("favicon");
+    favicon?.setAttribute("href", "../../public/pixel_shark_2.png");
+  }, []);
+
   return (
     <main className="flex h-full justify-center ">
       <div className="h-full w-full lg:max-w-4xl">{props.children}</div>
