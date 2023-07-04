34e68ec0e8865a222bcab6413f91e0d47b1d92ba
Michael Spencer <mwspencer75@gmail.com>
2023-07-04 14:01:28 -0700

    fixed the page layout

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -2,10 +2,8 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren<{}>) => {
   return (
-    <main className="flex h-screen justify-center">
-      <div className="h-full w-full overflow-y-scroll lg:max-w-4xl">
-        {props.children}
-      </div>
+    <main className="flex h-full justify-center ">
+      <div className="h-full w-full lg:max-w-4xl">{props.children}</div>
     </main>
   );
 };
