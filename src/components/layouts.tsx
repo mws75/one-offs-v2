f365548c5b9eb6bbecc2cc855e2ba146a6edc3ee
mws75 <mwspencer75@gmail.com>
2023-07-11 17:01:53 -0700

    updated the styling so the pink is the full screen

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -6,8 +6,10 @@ export const PageLayout = (props: PropsWithChildren<{}>) => {
   }, []);
 
   return (
-    <main className="flex h-screen justify-center ">
-      <div className="h-full w-full lg:max-w-4xl">{props.children}</div>
+    <main className="flex h-screen justify-center">
+      <div className="h-full w-full overflow-y-scroll lg:max-w-4xl">
+        {props.children}
+      </div>
     </main>
   );
 };
