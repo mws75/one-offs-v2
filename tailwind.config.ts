6e2b2c28520b4f54ddc4c5c52c871ee285136d9b
mws75 <mwspencer75@gmail.com>
2023-07-19 13:41:38 -0700

    removed the custom colors for now because it was removing other styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,6 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    colors: {
-      codeGrey: "#282C34",
-      purple: "#3f3cbb",
-    },
   },
   plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
