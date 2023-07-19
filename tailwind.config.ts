9a3a24da645e58cbe51fc153d84ac858d50b8961
mws75 <mwspencer75@gmail.com>
2023-07-19 13:16:20 -0700

    updated the styles of the code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      codeGrey: "#282C34",
+      purple: "#3f3cbb",
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
