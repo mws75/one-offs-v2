2bcd72121907860bdd18079ba27c828b2e18913f
mws75 <mwspencer75@gmail.com>
2023-07-19 13:57:13 -0700

    created custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,25 @@
 import { type Config } from "tailwindcss";
 
-export default {
+const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        codeGrey: {
+          DEFAULT: "#282C34",
+        },
+        purple: {
+          DEFAULT: "#3f3cbb",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
-} satisfies Config;
+};
+
+module.exports = config;
+
+// colors: {
+//   codeGrey: "#282C34",
+//   purple: "#3f3cbb",
+// },
